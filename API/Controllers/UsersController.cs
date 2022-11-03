80d88d11e9e3a31d6a6c18e1ff404cf8cb8696e9
Yazid Ismail <flight846@gmail.com>
2022-11-04 01:31:45 +0800

    Add users controller

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // api/users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() 
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        // api/users/{{id}}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
